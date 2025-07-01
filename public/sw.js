a70fbc862b66914daa38480366d8b3ce01f4ce39
rohit jha <rohit.1702jha@gmail.com>
2025-07-01 22:24:57 +0000

    Version code 1 has already been used. Try another version code.

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,14 @@
-// This basic service worker is enough to make the app installable.
-self.addEventListener('fetch', () => {
-  return;
+// A basic, no-op service worker.
+// This is just to prevent 404 errors for the service worker registration.
+
+self.addEventListener('install', (event) => {
+  console.log('Service Worker: Installing...');
+});
+
+self.addEventListener('activate', (event) => {
+  console.log('Service Worker: Activating...');
+});
+
+self.addEventListener('fetch', (event) => {
+  // We are not intercepting fetch requests in this basic setup.
 });
